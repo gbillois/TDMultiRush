335a027a7b550343adb6296cad57e7597d0f368c
gbillois <gerome.billois@gmail.com>
2026-03-01 14:37:54 +0100

    bug correction

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v3";
+const CACHE_VERSION = "v4";
 const STATIC_CACHE = `multipliRush-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `multipliRush-runtime-${CACHE_VERSION}`;
 
